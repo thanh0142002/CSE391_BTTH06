7da73fcf3ef94ce7a64fefe495e090f911ed0d6d
Nguyễn Văn Thành <thanh0142002@gmail.com>
2022-03-21 15:40:16 +0700

    đã chỉnh sửa

--- js/Dongho.js
+++ js/Dongho.js
@@ -1,18 +1,32 @@
+let arrayTime = {
+    "images/00.gif",
+    "images/01.gif",
+    "images/02.gif",
+    "images/03.gif",
+    "images/04.gif",
+    "images/05.gif",
+    "images/06.gif",
+    "images/07.gif",
+    "images/08.gif",
+    "images/09.gif",
+}
+
 function dongho() {
-    var today = new Date()
+
     var gio = today.getHours()
     var phut = today.getMinutes()
     var giay = today.getSeconds()
-    if (giay < 10) {
-        giay = "0" + giay
-    }
-    if (phut < 10) {
-        phut = "0" + phut
-    }
-    if (gio < 10) {
-        gio = "0" + gio
-    }
-    document.getElementById("time").innerHTML = gio + ":" + phut + ":" + giay
+
+    // document.getElementById("gio1").src = arrayTime[Math.floor(gio/10)]
+    // document.getElementById("gio2").src = arrayTime[gio/10]
+
+    // document.getElementById("phut1").src = arrayTime[Math.floor(phut/10)]
+    // document.getElementById("phut2").src = arrayTime[phut/10]
+    
+    // document.getElementById("giay1").src = arrayTime[Math.floor(giay/10)]
+    // document.getElementById("giay2").src = arrayTime[giay/10]
+
+
     setTimeout("dongho()", 1000)
     function cauchao() {
         if (0 <= gio < 12)
